f91d1e2145c17404af6affbadad5eb255b027ade
Richard Lambert <hello@richardlambert.dev>
2025-05-23 07:52:40 +0100

    :lipstick: add icon components

---
+++ src/components/icons/index.js
@@ -0,0 +1,6 @@
+export { BookmarkIcon } from './BookmarkIcon';
+export { CommentIcon } from './CommentIcon';
+export { EditIcon } from './EditIcon';
+export { LikeIcon } from './LikeIcon';
+export { SearchIcon } from './SearchIcon';
+export { FilterIcon } from './FilterIcon';
